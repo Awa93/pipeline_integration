068f35ab4f82ac29582bbeffc75a469dd018be0d
Awa93 <awadhesh.knit.1993@gmail.com>
2023-05-11 16:37:06 +0530

    changes

--- app/src/main/java/com/example/githubactiondemo/MainActivity.kt
+++ app/src/main/java/com/example/githubactiondemo/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val sum = addTwoNumbers(2, 3)
-        findViewById<TextView>(R.id.total).text = sum.toString()
+        findViewById<TextView>(R.id.total).text = "sum= "+ sum.toString()
     }
 
 
