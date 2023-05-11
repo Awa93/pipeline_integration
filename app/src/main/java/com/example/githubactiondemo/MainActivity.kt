0a37daebc11ac48c04e0c743b652cf28d6277134
Awa93 <awadhesh.knit.1993@gmail.com>
2023-05-11 13:38:53 +0530

    feature

--- app/src/main/java/com/example/githubactiondemo/MainActivity.kt
+++ app/src/main/java/com/example/githubactiondemo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.githubactiondemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sum = addTwoNumbers(2, 3)
+        findViewById<TextView>(R.id.total).text = sum.toString()
+    }
+
+
+    fun addTwoNumbers(a:Int,b:Int) : Int{
+        return a+b
     }
 }
